8239a10e2574cbb5f457b69151d0b062cc1cdf5e
octavePerraud <62344928+octavePerraud@users.noreply.github.com>
2020-10-19 19:19:32 +0200

    forma date

--- front/src/app/pages/add-user/add-user.component.ts
+++ front/src/app/pages/add-user/add-user.component.ts
@@ -20,16 +20,19 @@ export class AddUserComponent implements OnInit {
 
   onSubmit(ngForm: NgForm) {
     console.log(ngForm);
-    const user = defaultsDeep({
-      id: null,
-      firstName: ngForm.form.value.firstName,
-      lastName: ngForm.form.value.lastName,
-      email: ngForm.form.value.email,
-      age: ngForm.form.value.age,
-    });
+    if(ngForm.valid){
+      const user = defaultsDeep({
+        id: null,
+        firstName: ngForm.form.value.firstName,
+        lastName: ngForm.form.value.lastName,
+        email: ngForm.form.value.email,
+        age: ngForm.form.value.age,
+      });
 
-    this.userService.addUser(user).subscribe(user => console.log(user));
+      this.userService.addUser(user).subscribe(user => console.log(user));
 
-    this.router.navigateByUrl('/user');
+      this.router.navigateByUrl('/user');
+    }
+
   }
 }
