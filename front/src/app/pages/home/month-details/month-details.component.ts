cffa01df9e0b2a303924779fb5c26f73c8922369
Vitush19 <sutharsanvithusan@gmail.com>
2020-10-25 14:17:26 +0100

    dashboard display data from back

--- front/src/app/pages/home/month-details/month-details.component.ts
+++ front/src/app/pages/home/month-details/month-details.component.ts
@@ -1,18 +1,29 @@
-import {Component, Input, OnInit} from '@angular/core';
-import { MOTMS } from '../../../models/mock-motms';
-
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 @Component({
   selector: 'app-month-details',
   templateUrl: './month-details.component.html',
   styleUrls: ['./month-details.component.css']
 })
-export class MonthDetailsComponent implements OnInit {
-  motms=MOTMS;
-  @Input() selectedDate;
-  constructor() {
-  }
+export class MonthDetailsComponent implements OnChanges {
+  
+  @Input() month: string;
+  @Input() notesMonth: number[];
+  @Input() commentsMonth: string[];
+  comments: string[];
+  notes: number[];
+  result: number;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    const c = changes['commentsMonth'];
+    const r = changes['notesMonth'];
+    this.notes = r.previousValue;
+    if(this.notes !== undefined && this.notes.length > 0){
+      this.result = this.notes.reduce((p,c) => p+c, )/this.notes.length;
+    }
+    this.comments = c.previousValue;
   }
 
 }
