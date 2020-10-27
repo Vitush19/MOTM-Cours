88839a18b1bc75628eb1da57951c44f235421d7f
vitush <vithusan.sutharsan@epfedu.Fr>
2020-10-27 12:18:29 +0100

    getting mail to update for user

--- api/src/main/java/io/takima/demo/mail/MailController.java
+++ api/src/main/java/io/takima/demo/mail/MailController.java
@@ -1,11 +1,16 @@
 package io.takima.demo.mail;
+import io.takima.demo.entites.User;
+import io.takima.demo.entites.UserDAO;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/mails")
@@ -13,9 +18,10 @@ import java.util.List;
 @EnableAsync
 public class MailController {
     private final MailDAO mailDAO;
-
-    public MailController(MailDAO mailDAO) {
+    private final UserDAO userDAO;
+    public MailController(MailDAO mailDAO, UserDAO userDAO) {
         this.mailDAO = mailDAO;
+        this.userDAO = userDAO;
     }
 
     @GetMapping()
@@ -25,6 +31,35 @@ public class MailController {
         it.forEach(e -> mails.add(e));
 
         return mails;
+    }
+
+    @GetMapping("/{id}")
+    public Mail getUserUniq(@PathVariable Long id) {
+
+        boolean check = false;
+        Mail mailUser = null;
+        Optional<User> selectedUser = this.userDAO.findById(id);
+        if(selectedUser.isPresent()){
+            List<Mail> mails = selectedUser.get().getMailList();
+            for(int i = 0; i < mails.size(); i++){
+                Date today = new Date();
+                Mail m = mails.get(i);
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                String dateMail = sdf.format(m.getDate());
+                String dateToday = sdf.format(today);
+                String[] partDateMail = dateMail.split("-");
+                String[] partDateToday = dateToday.split("-");
+                if(partDateMail[1].equals(partDateToday[1])){
+                    check = true;
+                    mailUser = m;
+                }
+            }
+        }
+        if(check){
+            return mailUser;
+        }
+        return null;
+
     }
 
     @PostMapping()
