1f3673b2b1ea8709aa8191648c1e4ffcbd36f32d
Vitush19 <sutharsanvithusan@gmail.com>
2020-10-31 23:26:29 +0100

    sql insert data

--- api/initdb/2_DEFAULT_ENTRIES.sql
+++ api/initdb/2_DEFAULT_ENTRIES.sql
@@ -1,20 +1,9 @@
+INSERT INTO defaultdb.users (id, first_name, last_name, age, mail) VALUES (null, 'Octave', 'Perraud', '2000-12-12', 'octave.perraud@epfedu.fr');
+INSERT INTO defaultdb.users (id, first_name, last_name, age, mail) VALUES (null, 'Hugo', 'Lerondel', '1995-11-11', 'hugo.lerondel@epfedu.fr');
+INSERT INTO defaultdb.users (id, first_name, last_name, age, mail) VALUES (null, 'Vithusan', 'Sutharsan', '1998-12-19','sutharsanvithusan@epfedu.fr');
 
+INSERT INTO defaultdb.templates (id, msg_note, title) VALUES (null, 'Donnez votre note du mois :', 'Comment allez-vous ?');
 
-
-INSERT INTO defaultdb.users (id, first_name, last_name, age, mail) VALUES (null, 'Paul', 'Harrohide', '2000-12-12', 'sutharsanvithusan@gmail.com');
-INSERT INTO defaultdb.users (id, first_name, last_name, age, mail) VALUES (null, 'Harry', 'Covert', '1995-11-11', 'vithusan05@hotmail.fr');
-# INSERT INTO defaultdb.users (id, first_name, last_name, age) VALUES (3, 'Alain', 'Posteur', null);
-# INSERT INTO defaultdb.users (id, first_name, last_name, age) VALUES (4, 'Elvire', 'Debord', null);
-# INSERT INTO defaultdb.users (id, first_name, last_name, age) VALUES (5, 'Laurent', 'Barre', 41);
-# INSERT INTO defaultdb.users (id, first_name, last_name, age) VALUES (6, 'Homer', 'Cipourtoux', 28);
-# INSERT INTO defaultdb.users (id, first_name, last_name, age) VALUES (7, 'Gaston', 'Laplouz', null);
-# INSERT INTO defaultdb.users (id, first_name, last_name, age) VALUES (8, 'Gisèle', 'Detable', null);
-# INSERT INTO defaultdb.users (id, first_name, last_name, age) VALUES (9, 'Thomas', 'Ouaque', null);
-# INSERT INTO defaultdb.users (id, first_name, last_name, age) VALUES (10, 'Sacha', 'Telfrize', 23);
-
-INSERT INTO defaultdb.templates (id, msg_note, title) VALUES (null, 'Donnez votre note du mois :', 'Comment allez-vous ? ICI');
-
-INSERT INTO defaultdb.mails (id, note, comment, date, mail) VALUES (null, 4, 'Ca va bien mon gars!', '2020-09-25', 'vithusan05@hotmail.fr');
-INSERT INTO defaultdb.mails (id, note, comment, date, mail) VALUES (null, 5, 'Ca va bien !', '2020-09-24', 'sutharsanvithusan@gmail.com');
-# INSERT INTO defaultdb.mails (id, note, comment, date, mail) VALUES (null, 3, 'Ca va bien mon gars!', '2020-10-25', 'vithusan05@hotmail.fr');
-# INSERT INTO defaultdb.mails (id, note, comment, date, mail) VALUES (null, 4, 'Ca va bien !', '2020-10-24', 'sutharsanvithusan@gmail.com');
+INSERT INTO defaultdb.mails (id, note, comment, date, mail) VALUES (null, 4, 'Ca va bien mon gars!', '2020-09-25', 'octave.perraud@epfedu.fr');
+INSERT INTO defaultdb.mails (id, note, comment, date, mail) VALUES (null, 5, 'Ca va bien !', '2020-09-24', 'hugo.lerondel@epfedu.fr');
+INSERT INTO defaultdb.mails (id, note, comment, date, mail) VALUES (null, 1, 'Nul', '2020-10-25', 'sutharsanvithusan@gmail.com');
